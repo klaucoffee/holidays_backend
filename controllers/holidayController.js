54001148ac56c22ea512812169ddef9fa9e7e551
Krystle Lau <krystlelau90@gmail.com>
2022-04-02 15:51:12 +0800

    index route

--- controllers/holidayController.js
+++ controllers/holidayController.js
@@ -20,6 +20,17 @@ router.get("/", (req, res) => {
   res.send("holidays");
 });
 
+//* Index Route
+router.get("/", (req, res) => {
+  Holiday.find()
+    .then((holidays) => {
+      res.json(holidays);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
 //* Create Route
 router.post("/", async (req, res) => {
   try {
